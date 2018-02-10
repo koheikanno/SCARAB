23137e4774d3b962a1d8acde03f571705164b405
Kohei Kanno <koheikanno0@gmail.com>
2018-02-09 19:04:16 -0800

    Modified video.py - Fixed an issue that prevented the video window to show proper crosshair when the coordinates are out of or near the edges of the video feed.

--- src/das_fpv/src/video.py
+++ src/das_fpv/src/video.py
@@ -10,16 +10,25 @@ import cv2
 class Video:
 	def update_loc(self, x, y, alt):
 		# Called from callback_vfr_hud. Updates the overlaid landing location.
+		# self.x and self.y are location in pixel on the video feed. The arguments x and y are distance in ft 
 		# ft -> pixel to be implemented here.
 		# Assumptions: 4:3 640x480, 1 ft altitude -> 1 ft/480 px. Camera pointed directly down.
 		alt_pix = 1 / 480 # ft/px per foot of altitude
 		if alt > 0:
-			self.x = self.cam_height / 2 + x / alt_pix / alt
-			self.y = self.cam_width / 2 + y / alt_pix / alt
+			self.x = self.cam_width / 2 + y / alt_pix / alt
+			self.y = self.cam_height / 2 + x / alt_pix / alt
 		else:
 			# if the reported altitude is less than 0, leave the initial location (center of the screen)
-			self.x = self.cam_height / 2
-			self.y = self.cam_width / 2
+			self.x = self.cam_width / 2
+			self.y = self.cam_height / 2
+		if self.x < 0:
+			self.x = 0
+		if self.x > self.cam_width:
+			self.x = self.cam_width
+		if self.y < 0:
+			self.y = 0
+		if self.y > self.cam_height:
+			self.y = self.cam_height
 			
 	def overlay(self):
 		# This while loop runs continuously in a new thread while the video window is open
@@ -29,23 +38,30 @@ class Video:
 			y1 = self.y - self.crosshair_height / 2
 			y2 = self.y + self.crosshair_height / 2
 			if y1 < 0:
-				
-				y2 = y2 - y1
+				crosshair = self.crosshair[self.crosshair_height + y1:self.crosshair_height, :]
+				mask = self.mask[self.crosshair_height + y1:self.crosshair_height, :]
+				mask_inv = self.mask_inv[self.crosshair_height + y1:self.crosshair_height, :]
 				y1 = 0
-			if y2 > self.cam_width:
-				y1 = y1 - (y2 - self.cam_width)
-				y2 = self.cam_width
-			x1 = self.x - self.crosshair_height / 2
-			x2 = self.x + self.crosshair_height / 2
+			if y2 > self.cam_height:
+				crosshair = self.crosshair[0:self.crosshair_height - (y2 - self.cam_height), :]
+				mask = self.mask[0:self.crosshair_height - (y2 - self.cam_height), :]
+				mask_inv = self.mask_inv[0:self.crosshair_height - (y2 - self.cam_height), :]
+				y2 = self.cam_height
+			x1 = self.x - self.crosshair_width / 2
+			x2 = self.x + self.crosshair_width / 2
 			if x1 < 0:
-				x2 = x2 - x1
+				crosshair = self.crosshair[:, self.crosshair_width + x1:self.crosshair_width]
+				mask = self.mask[:, self.crosshair_width + x1:self.crosshair_width]
+				mask_inv = self.mask_inv[:, self.crosshair_width + x1:self.crosshair_width]
 				x1 = 0
-			if x2 > self.cam_height:
-				x1 = x1 - (x2 - self.cam_height)
-				x2 = self.cam_height
+			if x2 > self.cam_width:
+				crosshair = self.crosshair[:, 0:self.crosshair_width - (x2 - self.cam_width)]
+				mask = self.mask[:, 0:self.crosshair_width - (x2 - self.cam_width)]
+				mask_inv = self.mask_inv[:, 0:self.crosshair_width - (x2 - self.cam_width)]
+				x2 = self.cam_width
 			roi = frame[y1:y2, x1:x2]
-			roi_bg = cv2.bitwise_and(roi, roi, mask = self.mask_inv)
-			roi_fg = cv2.bitwise_and(self.crosshair, self.crosshair, mask = self.mask)
+			roi_bg = cv2.bitwise_and(roi, roi, mask = mask_inv)
+			roi_fg = cv2.bitwise_and(crosshair, crosshair, mask = mask)
 			dst = cv2.add(roi_bg, roi_fg)
 			frame[y1:y2, x1:x2] = dst
 			cv2.imshow('Video', frame)
