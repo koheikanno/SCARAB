5ee82483b40b464f8cff40b8821ed6d881cc1525
Kohei Kanno <koheikanno0@gmail.com>
2018-02-24 17:23:04 -0800

    Implemented a new overlay algorithm based on spherical projection from the camera location

--- src/das_fpv/src/video.py
+++ src/das_fpv/src/video.py
@@ -5,22 +5,27 @@
 
 # FPV/Video Overlay Integration
 
-import cv2
+import cv2, math
 
 class Video:
-	def update_loc(self, x, y, alt):
+	def update_loc(self, dist, div, alt):
 		# Called from callback_vfr_hud. Updates the overlaid landing location.
 		# self.x and self.y are location in pixel on the video feed. The arguments x and y are distance in ft 
 		# ft -> pixel to be implemented here.
-		# Assumptions: 4:3 640x480, 1 ft altitude -> 1 ft/480 px. Camera pointed directly down.
-		alt_pix = 1. / 480 # ft/px per foot of altitude
-		if alt > 0:
-			self.x = int(self.cam_width / 2 + alt_pix * y * alt)
-			self.y = int(self.cam_height / 2 + alt_pix * x * alt)
+		# Current assumptions: Camera aligned horizontally
+		# alt_pix = 1. / 480 # ft/px per foot of altitude
+		if alt > 0 and dist > 0:
+			phi = math.atan(alt/dist)
+			psi = math.atan(div/dist)
+		elif alt > 0 and dist < 0:
+			phi = math.radians(90)
+			psi = 0
 		else:
 			# if the reported altitude is less than 0, leave the initial location (center of the screen)
-			self.x = int(self.cam_width / 2)
-			self.y = int(self.cam_height / 2)
+			phi = 0
+			psi = 0
+		self.x = int(self.cam_width / 2 + psi * self.px_w_angle)
+		self.y = int(self.cam_height / 2 + phi * self.px_h_angle)
 		if self.x < 0:
 			self.x = 0
 		if self.x > self.cam_width:
@@ -87,6 +92,12 @@ class Video:
 		ret, frame = self.video_capture.read()
 		self.cam_height = int(frame.shape[:2][0])
 		self.cam_width = int(frame.shape[:2][1])
+		# Assumed 120 deg horizontal angle of view, 90 deg vertical angle of view
+		# no distortion
+		self.cam_height_angle = math.radians(120) # rad
+		self.cam_width_angle = math.radians(90) # rad 
+		self.px_h_angle = self.cam_height / self.cam_height_angle # px / rad
+		self.px_w_angle = self.cam_width / self.cam_width_angle # px / rad
 		self.crosshair_width = 64
 		self.crosshair_height = 64
 		self.crosshair = cv2.resize(self.img_crosshair, (self.crosshair_width, self.crosshair_height), interpolation = cv2.INTER_AREA)
